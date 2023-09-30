5c52c8c66723aa764e535d383e78fb7f829e79f4
YogeshTiwari10 <yogeshtiwari733@gmail.com>
2023-09-30 20:12:05 +0530

    mongoose to string

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
     try {
-      const conn = await mongoose.connect(process.env.MONGO_URI.toString());
+      const conn = await mongoose.connect(process.env.MONGO_URI).to;
       console.log(`MongoDB Connected ${conn.connection.host}`);
     } catch (error) {
       console.log(`Error : ${error.message}`);
